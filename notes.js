278959cfcf939a082b412fefeed01c92e903de06
ronniegikandi <kiiruronnie42@gmil.com>
2025-09-16 09:10:04 +0300

    second commit

---
+++ notes.js
@@ -0,0 +1,6 @@
+//note that arrow functions dont neeed curly brackets .... and if so ... if the block of code to be run by the function is just a single expression even the curly brackets are not really need ...
+const funct = (parameter1 , parameter2) => parameter1 + parameter2
+// automatically the arrow function will put the syntax its self and even auto return the value gotten from the expression 
+// this logic still works for the parameter...funct. if one parameter is passed then theres no need to add brackets
+const func = x => x*x
+
